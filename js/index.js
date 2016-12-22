75f4fcfe68cefed11bef699f401ec233f15243ae
Linai <linai081795@gmail.com>
2016-12-22 14:29:48 +0800

    add-milliseconds

--- js/index.js
+++ js/index.js
@@ -5,8 +5,9 @@ function time() {
     //console.log(h);
     var m = Day.getMinutes();
     var s = Day.getSeconds();
+    var mi=Day.getMilliseconds();
     //console.log(s);
-    document.getElementById("time1").innerHTML = "<h1>现在时间:" + h + "点:" + m + "分:" + s + "秒</h1>";
+    document.getElementById("time1").innerHTML = "<h1>现在时间:" + h + "点:" + m + "分:" + s + "秒:"+mi+"</h1>";
     //console.log(time());
     var mytime = setTimeout("time()", 1000);
 }

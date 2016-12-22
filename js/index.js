0906ea816f1b94e744143b5ae86b8670a98a08b8
Linai <linai081795@gmail.com>
2016-12-22 14:26:21 +0800

    分开

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 'use static'
+function time() {
+    var Day = new Date();
+    var h = Day.getHours();
+    //console.log(h);
+    var m = Day.getMinutes();
+    var s = Day.getSeconds();
+    //console.log(s);
+    document.getElementById("time1").innerHTML = "<h1>现在时间:" + h + "点:" + m + "分:" + s + "秒</h1>";
+    //console.log(time());
+    var mytime = setTimeout("time()", 1000);
+}
